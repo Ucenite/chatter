17e78e19a2322d7fa9898f9bb38c72460631d539
Cecilia Slawik <cecilia.slawik@x-131-212-234-98.uofm-secure.wireless.umn.edu>
2015-03-07 13:54:27 -0600

    Changed to use hiccup.

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -1,11 +1,26 @@
 (ns chatter.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [hiccup.page :as page]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" []
+       (page/html5
+        [:head
+         [:title "chatter"]]
+        [:body
+         [:h1 "Our Chat Application"]]))
   (route/not-found "Not Found"))
+
+(defn generate-message-view
+  "This generates the HTML for displaying messages."
+  []
+  (page/html5
+   [:head
+    [:title "chatter"]]
+   [:body
+    [:h1 "Our Chat Application"]]))
 
 (def app
   (wrap-defaults app-routes site-defaults))
